5f32da85293a28f84ee36b0f315b3415c4b28cb5
SnubStumpy2007 <joshua192007@gmail.com>
2024-03-07 17:34:13 -0500

    update

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const submit = () => {
     const ans = document.getElementById("answer").value
-    document.getElementById("bio").innerHTML = ans
+    console.log(ans)
+
+    if(ans === "Jude Mathis") {
+        const intro = document.getElementById("bio")
+        intro.textContent += "Jude Mathis is a Med Student"
+        const image = document.getElementById("character-image")
+        image.src = "jude.jpg"        
+    }
 }
